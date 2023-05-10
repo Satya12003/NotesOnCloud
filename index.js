26523d5e1df4667befc8c9b6a655d4bc2eeadcc0
Satya12003 <114014184+Srinija-19@users.noreply.github.com>
2023-05-11 03:54:40 +0530

    changes

--- index.js
+++ index.js
@@ -1,27 +1,25 @@
-const connectToMongo = require('./db')
-var cors = require('cors')
+const dotenv = require("dotenv");
+dotenv.config();
+const connectToMongo = require("./db");
+var cors = require("cors");
 
-const express = require('express')
-
+const express = require("express");
 
 //connection to mongo db in db.js
 connectToMongo();
 
 //dont worry about rest of the code expect routes which u have to write them on ur own but the rest of them u can get them from the express website
-const app = express()
-const port = 3333
+const app = express();
+const port = process.env.PORT || 3333 ;
 
 //this is a middleware which is used to parse the input and output in terms of json it is quite important
-app.use(cors())
-app.use(express.json())
-
+app.use(cors());
+app.use(express.json());
 
 //Available routes ==> go to routes folder for more info
-app.use('/api/auth',require("./routes/auth"))
-app.use('/api/notes',require("./routes/notes"))
-
-
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/notes", require("./routes/notes"));
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
