b6664d9a9204031a03b4a9d6a807f2a735106cd8
Satya12003 <114014184+Srinija-19@users.noreply.github.com>
2023-05-11 19:59:51 +0530

    changes36

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const dotenv = require("dotenv");
 dotenv.config();
 const connectToMongo = require("./db");
+var cors = require("cors");
 
 const express = require("express");
 
@@ -12,17 +13,27 @@ const app = express();
 const port = process.env.PORT || 3333;
 
 //this is a middleware which is used to parse the input and output in terms of json it is quite important
+app.use(cors());
 app.use(express.json());
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "*");
-  if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
-    return res.status(200).json({});
-  }
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  // Pass to next layer of middleware
   next();
 });
-
 //Available routes ==> go to routes folder for more info
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/notes", require("./routes/notes"));
